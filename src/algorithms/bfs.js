fab7801f9a89a2fdc673df2924704f3f10111665
Aniruddh Balram <abalram1@umd.edu>
2023-03-28 11:00:54 -0400

    Added BFS algorithm

--- src/algorithms/bfs.js
+++ src/algorithms/bfs.js
@@ -1,3 +1,61 @@
 export function BFS(grid, startNode, finishNode) {
-    console.log("BFS");
+  let unVisitedNodes = [];
+  let visitedNodes = [];
+  startNode.distance = 0;
+  console.log("start node: ", [startNode.row, startNode.col], " finishNode", [
+    finishNode.row,
+    finishNode.col,
+  ]);
+  unVisitedNodes.push(startNode);
+
+  while (unVisitedNodes.length !== 0) {
+    if (nextNode === finishNode) return [true, visitedNodes];
+
+    console.log("length of unvisited nodes: ", unVisitedNodes.length);
+    const nextNode = unVisitedNodes.shift();
+    console.log(nextNode.row, nextNode.col);
+    if (nextNode.isWall) continue;
+    if (!nextNode.isVisited) {
+      nextNode.isVisited = true;
+      visitedNodes.push(nextNode);
+      let { col, row } = nextNode;
+      if (row > 0) {
+        if (!grid[row - 1][col].isVisited) {
+          grid[row - 1][col].previousNode = nextNode;
+          unVisitedNodes.push(grid[row - 1][col]);
+        }
+      }
+      if (row < grid.length - 1) {
+        if (!grid[row + 1][col].isVisited) {
+          grid[row + 1][col].previousNode = nextNode;
+          unVisitedNodes.push(grid[row + 1][col]);
+        }
+      }
+      if (col > 0) {
+        if (!grid[row][col - 1].isVisited) {
+          grid[row][col - 1].previousNode = nextNode;
+          unVisitedNodes.push(grid[row][col - 1]);
+        }
+      }
+      if (col < grid[0].length - 1) {
+        if (!grid[row][col + 1].isVisited) {
+          grid[row][col + 1].previousNode = nextNode;
+          unVisitedNodes.push(grid[row][col + 1]);
+        }
+      }
+    }
+  }
+  return [false, visitedNodes];
+}
+
+// Backtracks from the finishNode to find the shortest path.
+// Only works when called *after* the dijkstra method above.
+export function getNodesInShortestPathOrder(finishNode) {
+  const nodesInShortestPathOrder = [];
+  let currentNode = finishNode;
+  while (currentNode !== null) {
+    nodesInShortestPathOrder.unshift(currentNode);
+    currentNode = currentNode.previousNode;
+  }
+  return nodesInShortestPathOrder;
 }
